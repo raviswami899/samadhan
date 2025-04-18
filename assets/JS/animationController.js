0a8000a184db5a1c004bb4e5bdd16d490bbfe150
Ravi Swami <raviswami@MacBookAir.lan>
2025-04-18 14:02:50 +0530

    animation window changes

--- assets/JS/animationController.js
+++ assets/JS/animationController.js
@@ -2,8 +2,10 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         entry.target.classList.add('active');
+        entry.target.classList.remove('inactive');
       } else {
         entry.target.classList.remove('active');
+        entry.target.classList.add('inactive');
       }
     });
   }, {
