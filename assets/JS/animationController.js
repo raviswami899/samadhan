0a505820101bbaa5ba222b8f8e2209bf4768678a
Ravi Swami <raviswami@MacBookAir.lan>
2025-04-18 19:35:12 +0530

    adding footer section

--- assets/JS/animationController.js
+++ assets/JS/animationController.js
@@ -2,10 +2,9 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         entry.target.classList.add('active');
-        entry.target.classList.remove('inactive');
       } else {
-        entry.target.classList.remove('active');
-        entry.target.classList.add('inactive');
+        // entry.target.classList.remove('active');
+        // entry.target.classList.add('inactive');
       }
     });
   }, {
